1ead048f4aa8c4f631dc00e2515b8e0ca41f7bff
ajskateboarder <81255936+lemystic@users.noreply.github.com>
2023-03-15 15:32:48 +0000

    refactor: remove usages of writing to paths
    time to modify tests

---
+++ app/src/App.py
@@ -0,0 +1,8 @@
+from spylt import require_svelte
+
+app = require_svelte("./src/App.svelte")
+
+
+@app.backend()
+def say_hello(name: str):
+    return f"Hello {name}"
---
+++ app/src/App.svelte
@@ -0,0 +1,16 @@
+<!-- point ./src/App.py:app -->
+<script>
+    const say_hello = async (name) => { 
+        request = await fetch(`/api/say_hello?name=${name}`)
+        json = await request.json()
+        return json.response
+    }
+
+    let greeting
+    let name = "Bob"
+</script>
+<main>
+    <input type="text" bind:value={name}>
+    <button on:click={(async function() { greeting = await say_hello(name) })()}>Say hello</button>
+    <p>{greeting}</p>
+</main>
